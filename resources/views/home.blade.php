cfd62e0f3aa47c230c52827685a763b9d13eb85c
maricamola <maricamola98@libero.it>
2023-06-12 16:39:11 +0200

    PageController added

---
+++ resources/views/home.blade.php
@@ -0,0 +1,15 @@
+@extends('layouts.main')
+
+@section('title')
+    Home
+@endsection
+
+@section('content')
+
+<main>
+<div class="container">
+    <h1>Home</h1>
+</div>
+</main>
+
+@endsection
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,14 @@
+<header>
+    <div class="logo">
+        <img src="/img/dc-logo.png" alt="Logo">
+    </div>
+
+    <ul class="nav">
+        <li class="nav-item">
+            <a class="nav-link" href="{{ route('home') }}">Home</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="{{ route('contacts') }}">Contatti</a>
+        </li>
+    </ul>
+</header>
